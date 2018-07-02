622a6ffb675caa842a23eb1ef9cb740fc84e84a3
Michael Zampino <michaelzampino@gmail.com>
2018-07-02 12:09:23 -0400

    quick base server add

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/ping", ping)
+
+	port := ":8080"
+	log.Println("Starting server on port", port)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "<h1>Test Server</h1>")
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+	//	w.Header().Set("Content-type", "text/html")
+	fmt.Fprint(w, "pong\n")
+}
